6e51a4db01dae4e20cde8f4b5f43c5c1fc3f9428
Franco Morales Castro <franco.morales@outlook.com>
2026-09-08 14:48:41 -0300

    Add 'website' app to installed apps and create initial migration for Proyecto model

--- website/models.py
+++ website/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Proyecto(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    tecnolgia = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.titulo
