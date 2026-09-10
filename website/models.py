c6b482a885d643fe90964db49d838ad7b6641a63
Franco Morales Castro <franco.morales@outlook.com>
2026-09-10 17:41:04 -0300

    Relaciones entre modelos

--- website/models.py
+++ website/models.py
@@ -1,10 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Tecnologia(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+    
 class Proyecto(models.Model):
     titulo = models.CharField(max_length=100)
     descripcion = models.TextField()
-    tecnolgia = models.CharField(max_length=100)
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.CASCADE, related_name='proyectos', null=True, blank=True
+    )
+    tecnologias = models.ManyToManyField(
+        Tecnologia,
+        related_name='proyectos',
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return self.titulo
